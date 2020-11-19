124cfa15f5ac1d6432608a15e574756013e92744
unknown <alejandrojzeballos@gmail.com>
2020-11-18 20:18:13 -0400

    insertar foro terminado

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -34,7 +34,17 @@ class Home extends CI_Controller {
 		$u_nombre=$this->input->post('u_nombre');
 		$u_email=$this->input->post('u_email');
 		$u_comentario=$this->input->post('u_comentario');
-		echo 'asdasdas';
-		echo "$u_nombre <br> data $u_email <br> aqui $u_comentario";
+		$foro_detalles = array(
+			'u_nombre' => $u_nombre,
+			'u_email' => $u_email,
+			'u_comentario' => $u_comentario
+		);
+		var_dump($foro_detalles);
+		if(!$this->Foro_model->insertarUsuario($foro_detalles)){
+			$this->output->set_status_header(500);
+            echo json_encode(array('msg' => 'Error al crear comentario'));
+            exit;
+		}
+		redirect('home/foroData','refresh');
 	}
 }
